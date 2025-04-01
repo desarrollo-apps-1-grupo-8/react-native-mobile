8984f2370560e22ade27cda2204a04a59e3ecde7
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2025-03-31 23:38:04 -0300

    initialized project

---
+++ src/main/java/ar/edu/uade/desa1/Desa1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.desa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/uade/desa1/Desa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.desa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
