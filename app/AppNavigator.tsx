158b68f33ca936295a7330995f4efd1e7c49d184
Agustin Hess <108557104+Hessoide@users.noreply.github.com>
2025-07-06 13:44:12 -0300

    Funcionalidad para clickear la notificacion y que redireccione segun corresponda
    Funcionalidad para clickear la notificacion y que redireccione segun corresponda

--- app/AppNavigator.tsx
+++ app/AppNavigator.tsx
@@ -1,31 +1,51 @@
 import Loader from '@/components/Loader';
 import NetworkStatus from '@/components/ui/NetworkStatus';
 import { useSession } from '@/context/SessionContext';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native';
+import * as Notifications from 'expo-notifications';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useRef } from 'react';
 import Toast from 'react-native-toast-message';
 import AppStack from './AppStack';
 import AuthStack from './AuthStack';
 
-
 export default function AppNavigator() {
   const { session, isLoading } = useSession();
+  const navigationRef = useRef<NavigationContainerRef<any>>(null);
+
+  useEffect(() => {
+  const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+    const data = response.notification.request.content.data;
+    console.log('Notificación tocada, data:', data);
+
+    //redireccion a shipments
+    if (data?.target === 'Shipments') {
+      navigationRef.current?.navigate('Shipments');
+    }
+
+    //redireccion a mis envios
+    if (data?.target === 'MyRoutes') {
+      navigationRef.current?.navigate('MyRoutes');
+    }
+  });
+
+  return () => subscription.remove();
+}, []);
 
   if (isLoading) {
     return <Loader />;
   }
 
-
-    return (
-      <>
-        <StatusBar style="light" translucent={true} />
-        <NetworkStatus />
-        <NavigationContainer>
-          {session ? <AppStack /> : <AuthStack />}
-        </NavigationContainer>
-        <Toast />
-      </>
-    );
-
+  return (
+    <>
+      <StatusBar style="light" translucent={true} />
+      <NetworkStatus />
+      <NavigationContainer ref={navigationRef}>
+        {session ? <AppStack /> : <AuthStack />}
+      </NavigationContainer>
+      <Toast />
+    </>
+  );
 }
+
 
